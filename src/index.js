e8f01451654d216793a0eca2c52de61fdf1b2502
Lyan <lyanbrito613@gmail.com>
2025-01-14 19:12:17 -0300

    feat:added-delete-operation

--- src/index.js
+++ src/index.js
@@ -23,12 +23,23 @@ const Film = mongoose.model('Film' ,{
 // res seria a resposta desse requisição
 // .post usado para criar ou salvar um novo dado
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+// aqui estou dizendo para ele me retornar todos os filmes que tiver
+app.get('/', async (req, res) => {
+  const Films = await Film.find()
+  res.send(Films)
+})
+
+// com .delete e .put devemos passar o id por parâmetro
+// o id é criado com o metodo post, ao adicionar um novo Film no BD
+app.delete('/:id', async(req, res) =>{
+  const film = await Film.findByIdAndDelete(req.params.id)
+  // .findById vai apenas retornar o array daquela id, porem não vai de fato deleta-lo. então usamons findByIdAndDelete
+  res.send(film)
 })
 
 // aqui estou criando um novo "Film", e vou fazer uma requisição no body, pra trazer novos dados
-// por ser uma operação asincrona, e devemosesperar o banco de dados retornar a função, adicionamos async e awayt
+// por ser uma operação asincrona, e devemos esperar o banco de dados retornar a função, adicionamos async e awayt
 
 app.post('/', async (req, res) => {
   const newFilm = new Film({
